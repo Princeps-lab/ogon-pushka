76a851a45deac1de5ccaee3643e52a4c9df67566
novgorodskii <novgorodskiji@gmail.com>
2020-07-07 21:46:58 +0300

    add extra content

--- components/Extra/Extra.js
+++ components/Extra/Extra.js
@@ -1,18 +1,39 @@
 import Button from '../Button';
 import style from "./extra.module.css";
 
+const Top = () => {
+  return (
+    <div className={style.top}>
+      <div className={style.topTitle}>OGON PUSHKA</div>
+      <p>
+        Это то, в чем ты точно будешь сворачивать головы этим летом.<br/>
+        Сочная подборка максимально востребованных цветов и стилей.
+      </p>
+      <Button text="Подробнее"/>
+    </div>
+  )
+};
+
+const ExtraItem = ({url}) => {
+  return(
+    <div style={{"background" : `url(${url}) center / cover`}} />
+  )
+};
+
+const FullItem = ({url}) => <div style={{"background" : `url(${url}) center / cover`}} />;
+
 const Extra = () => {
   return(
     <div className={style.extra}>
-      <div className={style.extraLeft}>
-        <div className={style.top}>
-          <div className={style.topTitle}>OGON PUSHKA</div>
-          <p>
-            Это то, в чем ты точно будешь сворачивать головы этим летом.<br/>
-            Сочная подборка максимально востребованных цветов и стилей.
-          </p>
-          <Button text="Подробнее"/>
+      <div className={style.left}>
+        <Top />
+        <div className={style.bottom}>
+          <ExtraItem url="/images/extraLeft.jpg"/>
+          <ExtraItem url="/images/extraRight.jpg"/>
         </div>
+      </div>
+      <div  className={style.right}>
+        <FullItem url="/images/extra_full.jpg" />
       </div>
     </div>
   );
