fbe3a53671a2aabf832cad7b3ec582581d956cd2
novgorodskii <novgorodskiji@gmail.com>
2020-07-14 06:11:34 +0300

    add slider catalog

--- components/MainCatalog/MainCatalog.js
+++ components/MainCatalog/MainCatalog.js
@@ -6,8 +6,8 @@ import style from './mainCatalog.module.css';
 const CatalogItem = ({url, title}) => {
   return(
     <div className={style.catalogItem} style={{"background" : `url(${url}) center / cover`}}>
-      <div className={style.itemTitle}>{title}</div>
       <div className={style.button}>
+        <div className={style.itemTitle}>{title}</div>
         <Button text="Подробнее" />
       </div>
     </div>
--- components/SimpleSlider/SimpleSlider.js
+++ components/SimpleSlider/SimpleSlider.js
@@ -1,21 +1,57 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
+
+import Button from '../Button';
 
 import './SimpleSlider.module.css';
+
+SwiperCore.use([Navigation]);
+
+import style from './SimpleSlider.module.css';
+
+const ItemSlider = ({url, title}) => {
+  return (
+    <div className={style.catalogItem} style={{"background" : `url(${url}) center / cover`}}>
+      <div className={style.button}>
+        <div className={style.itemTitle}>{title}</div>
+        <Button text="Подробнее" />
+      </div>
+    </div>
+  );
+};
 
 const SimpleSlider = () => {
   return (
     <Swiper
-      spaceBetween={0}
       slidesPerView={4}
-      onSlideChange={() => console.log('slide change')}
+      navigation
       onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
+      <SwiperSlide>
+        <ItemSlider url="/images/catalog/item1.jpg" title="Paragraphs" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <ItemSlider url="/images/catalog/item2.jpg" title="Paragraphs"/>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <ItemSlider url="/images/catalog/item3.jpg" title="Paragraphs" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <ItemSlider url="/images/catalog/item4.jpg" title="Paragraphs" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <ItemSlider url="/images/catalog/item1.jpg" title="Paragraphs" />
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <ItemSlider url="/images/extraLeft.jpg" title="Paragraphs" />
+      </SwiperSlide>
 
     </Swiper>
   )
