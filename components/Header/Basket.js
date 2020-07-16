95b56174c0a2334a3e2999fbd71847f37b3b2a78
novgorodskii <novgorodskiji@gmail.com>
2020-07-16 13:14:47 +0300

    add basket

--- components/Header/Basket.js
+++ components/Header/Basket.js
@@ -1,10 +1,23 @@
-import style from "./header.module.css"
+import style from "./header.module.css";
+import React, {useState} from 'react';
 
 const Basket = () => {
+
+  const [basketShow, setShow] = useState(false);
+
   return (
     <div className={style.icons}>
       <div><img src="/icons/heart.svg"></img></div>
-      <div><img src="/icons/basket.svg"></img></div>
+      <div onClick={() => setShow(true)}><img src="/icons/basket.svg"></img></div>
+
+      <div className={basketShow ? style.basket : style.basketOff}>
+        <div className={style.basketHeader}>
+          <h2>Корзина</h2>
+          <div onClick={() => setShow(false)}>
+            <img src="/icons/close.svg" alt="close" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,4 @@
 import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-
 import Header from './Header';
 import Footer from './Footer';
 
