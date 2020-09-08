b99b8114b724fbcbbcfdefd7578acec06b8891d6
Новгородский Игорь <novgorodskii@iMac-Novgorodskij.local>
2020-09-08 20:11:56 +0300

    add response contact page

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,21 @@
+import React, {useContext} from 'react';
+import {ProductsContext} from '../context/context.js';
+
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import SocialBlock from '../components/SocialBlock';
 import AboutContent from '../components/AboutContent';
+import MobSocial from '../components/MobSocial';
 
 const AboutUs = () => {
+  const context = useContext(ProductsContext);
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <AboutContent/>
-      <SocialBlock />
+      { context.desktop ? <SocialBlock /> : <MobSocial /> }
     </Layout>
   )
 };
--- pages/favorites.js
+++ pages/favorites.js
@@ -1,17 +1,22 @@
+import React, {useContext} from 'react';
+import {ProductsContext} from '../context/context.js';
+
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import Extra from '../components/Extra';
+import MobExtra from '../components/MobExtra';
 import FavoritesContent from '../components/FavoritesContent';
 
 const Favorites = () => {
+
+  const context = useContext(ProductsContext);
   return (
     <Layout>
-      <div>Favorites</div>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <FavoritesContent/>
-      <Extra />
+      { context.desktop ? <Extra /> : <MobExtra /> }
     </Layout>
   );
 };
