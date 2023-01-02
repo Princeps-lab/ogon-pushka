66761a6187025ea59e38d7513ad21f0ac659ed2c
Novgorodskii Igor <novgorodskiji@gmail.com>
2023-01-03 01:16:44 +0200

    update liq

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  images: {
+    domains: ['ogonpushka-production.s3.eu-central-1.amazonaws.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
 }
-
-module.exports = nextConfig
